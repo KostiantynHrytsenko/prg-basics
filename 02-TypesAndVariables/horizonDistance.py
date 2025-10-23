8f2fa90044aa19c347971ad0aadcbb62d8256ca5
KostiantynHrytsenko <kostiahrytsenko@gmail.com>
2025-10-23 11:13:16 +0200

    10.23.25

---
+++ 02-TypesAndVariables/horizonDistance.py
@@ -0,0 +1,7 @@
+import math
+
+height = 2
+
+distance = math.sqrt(height) * 3.57
+
+print(distance)
---
+++ 02-TypesAndVariables/valueSwap.py
@@ -0,0 +1,11 @@
+x = 5
+y = 342
+z = 0 # additional, auxiliary variable
+print("Before swapping: x=", x, "y=", y)
+
+# swap the value
+z = x
+x = y
+y = z
+
+print("AFter swapping: x=", x, "y=", y)
